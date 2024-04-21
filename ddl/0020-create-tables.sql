7682bd2422b8247c6f332093e4f7d38f990bb4ca
Mitsutoshi Nakano <ItSANgo@gmail.com>
2024-04-22 00:16:26 +0900

    fix DDL bug

--- ddl/0020-create-tables.sql
+++ ddl/0020-create-tables.sql
@@ -2,24 +2,26 @@ CREATE ROLE apache PASSWORD 'vagrant' LOGIN;
 
 CREATE TABLE tickets (
     id INTEGER,
-    subject VARCHAR(997) IS NOT NULL,
+    subject VARCHAR(997) NOT NULL,
     affiliation VARCHAR(126),
     pronunciation_for_affiliation VARCHAR(126),
     abbreviation_for_affiliation VARCHAR(126),
-    reporter VARCHAR(126) IS NOT NULL,
+    reporter VARCHAR(126) NOT NULL,
     pronunciation_for_reporter VARCHAR(126),
-    address VARCHAR(126) IS NOT NULL,
+    address VARCHAR(126) NOT NULL,
     pronunciation_for_address VARCHAR(126),
     found_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     report_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     last_modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     closed_at TIMESTAMP,
-    status VARCHAR(126) IS NOT NULL,
-    description VARCHAR(10485760) IS NOT NULL,
+    status VARCHAR(126) NOT NULL,
+    description VARCHAR(10485760) NOT NULL,
     results VARCHAR(10485760),
-    creator VARCHAR(126) IS NOT NULL,
-    updater VARCHAR(126) IS NOT NULL,
+    creator VARCHAR(126) NOT NULL,
+    updater VARCHAR(126) NOT NULL,
     create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)    
 );
+
+GRANT SELECT, INSERT, UPDATE ON tickets TO apache;
